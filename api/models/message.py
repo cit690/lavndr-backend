e7ba4026a36c967c59c4106a18f266dad30c6c9e
tyebean <flower.webster@gmail.com>
2022-04-12 13:14:02 -0400

    work on priv msg create view/route

--- api/models/message.py
+++ api/models/message.py
@@ -5,8 +5,8 @@ class Message(db.Model):
   __tablename__ = 'messages'
   id = db.Column(db.Integer, primary_key=True)
   sent_at = db.Column(db.DateTime, default=datetime.utcnow)
-  recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  recipient_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
+  sender_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
   content = db.Column(db.String(500))
   # profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
 
--- api/views/message.py
+++ api/views/message.py
@@ -4,6 +4,7 @@ from api.middleware import login_required, read_token
 
 from api.models.db import db
 from api.models.message import Message
+from api.models.profile import Profile
 
 messages = Blueprint('messages', 'message')
 
@@ -11,6 +12,7 @@ messages = Blueprint('messages', 'message')
 @messages.route('/', methods=["POST"])
 @login_required
 def create():
+  profile = Profile.query.filter_by(name=recipient).first_or_404()
   data = request.get_json()
   # below, we retrieve a user's user data with the read_token middleware function, and assign that to a variable called user
   user = read_token(request)
@@ -20,6 +22,35 @@ def create():
   db.session.add(message)
   db.session.commit()
   return jsonify(message.serialize()), 201
+
+# ***** following the tutorial that i sent to claire 
+# im on the part where they say
+# "Next I'm going to add a new /send_message/<recipient> route"
+# they use a flask form but since we're using react, we can try to incorporate those parts in a different way
+
+# @bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+# @login_required
+# def send_message(recipient):
+#     user = User.query.filter_by(username=recipient).first_or_404()
+#     form = MessageForm()
+#     if form.validate_on_submit():
+#         msg = Message(author=current_user, recipient=user,
+#                       body=form.message.data)
+#         db.session.add(msg)
+#         db.session.commit()
+#         flash(_('Your message has been sent.'))
+#         return redirect(url_for('main.user', username=recipient))
+#     return render_template('send_message.html', title=_('Send Message'),
+#                            form=form, recipient=recipient)
+
+
+
+
+
+
+
+
+
 
 # indexing a msg - @login_required
 @messages.route('/', methods=["GET"])
