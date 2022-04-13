cd930b0ff092a23f7b6f956670344af38d09ccdd
tyebean <flower.webster@gmail.com>
2022-04-13 15:00:24 -0400

    add primary/secondary keys to msg relationship in profile mod

--- api/models/message.py
+++ api/models/message.py
@@ -1,22 +1,14 @@
 from datetime import datetime
 from api.models.db import db
 
-
 class Message(db.Model):
   __tablename__ = 'messages'
   id = db.Column(db.Integer, primary_key=True)
-
   content = db.Column(db.String(500))
   sent_at = db.Column(db.DateTime, default=datetime.utcnow)
 
-# * trying this
   recipient_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
   sender_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
-
-# * Relationships:
-# * trying this
-  # recipients = db.relationship("Recipient", secondary="associations")
-  # senders = db.relationship("Senders", secondary="associations")
 
   def __repr__(self):
     return f"Message('{self.id}', '{self.message}'"
--- api/models/profile.py
+++ api/models/profile.py
@@ -7,11 +7,6 @@ class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    # sender_id = db.Column(db.Integer, db.ForeignKey('recipients.id'))
-    # recipient_id = db.Column(db.Integer, db.ForeignKey('senders.id'))
-
-    # message_id = db.Column(db.Integer, db.ForeignKey('messages.id'))
-
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     name = db.Column(db.String(100), nullable=False)
     profile_picture = db.Column(db.String())
@@ -21,11 +16,9 @@ class Profile(db.Model):
     location = db.Column(db.String)
     vibe_check = db.Column(db.String(200))
     bio = db.Column(db.String(500))
-    # meal = db.Column('meal', db.Enum('B', 'L', 'D', name='meal_type'))
     sun_sign = db.Column('sun_sign', db.Enum('Ari', 'Tau', 'Gem', 'Can', 'Leo', 'Vir', 'Lib', 'Sco', 'Sag', 'Cap', 'Aqu', 'Pis', name='sun_type'))
     moon_sign = db.Column('moon_sign', db.Enum('Ari', 'Tau', 'Gem', 'Can', 'Leo', 'Vir', 'Lib', 'Sco', 'Sag', 'Cap', 'Aqu', 'Pis', name='moon_type'))
     rising_sign = db.Column('rising_sign', db.Enum('Ari', 'Tau', 'Gem', 'Can', 'Leo', 'Vir', 'Lib', 'Sco', 'Sag', 'Cap', 'Aqu', 'Pis', name='rising_type'))
-
     profile_picture = db.Column(db.String())
     gender_identity = db.Column(db.String())
     orientation = db.Column(db.String())
@@ -35,7 +28,7 @@ class Profile(db.Model):
     is_sober = db.Column(db.Boolean())
 
     # * Relationships:
-    # messages = db.relationship("Message", secondary="associations")
+    messages = db.relationship("Message", secondary="messages", primaryjoin="Message.recipient_id==Profile.id", secondaryjoin="Message.sender_id==Profile.id")
 
     def __repr__(self):
       return f"Profile('{self.id}', '{self.name}'"
@@ -43,38 +36,7 @@ class Profile(db.Model):
     def serialize(self):
       profile = {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-      messages = [message.serialize() for message in self.messages]  # <=== Associate ===
-      profile['messages'] = messages  # <=== Associate  ===
+      messages = [message.serialize() for message in self.messages]
+      profile['messages'] = messages 
 
       return profile
-
-
-# class Recipient(db.Model):
-#   __tablename__ = 'recipients'
-#   id = db.Column(db.Integer, primary_key=True)
-
-#   messages = db.relationship("Message", secondary="associations")
-
-#   def __repr__(self):
-#     return f"Recipients('{self.id}', '{self.recipient}'"
-
-#   def serialize(self):
-#     recipient = {c.name: getattr(self, c.name) for c in self.__table__.columns}
-#     messages = [message.serialize() for message in self.messages]  # <=== Associate ===
-#     recipient['messages'] = messages  # <=== Associate  ===
-#     return recipient
-
-# class Sender(db.Model):
-#   __tablename__ = 'senders'
-#   id = db.Column(db.Integer, primary_key=True)
-
-#   messages = db.relationship("Message", secondary="associations")
-
-#   def __repr__(self):
-#     return f"senders('{self.id}', '{self.sender}'"
-
-# def serialize(self):
-#     sender = {c.name: getattr(self, c.name) for c in self.__table__.columns}
-#     messages = [message.serialize() for message in self.messages]  # <=== Associate ===
-#     sender['messages'] = messages  # <=== Associate  ===
-#     return sender
